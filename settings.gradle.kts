41c063a3314a04d24967d75ce0940fb0bd1ca419
Jumman04 <arsharifuddinjumman04@gmail.com>
2024-05-28 17:13:33 +0600

    Optimized whole code.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven ("https://jitpack.io")
+        maven("https://jitpack.io")
     }
 }
 
